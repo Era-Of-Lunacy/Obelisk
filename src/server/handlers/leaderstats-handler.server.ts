3be79d4f919e3e0449bcc145261aa16d11c8e962
Waffle <csshin9928@gmail.com>
2026-01-07 04:44:57 +0900

    feat: implement leaderstats handler

---
+++ src/server/handlers/leaderstats-handler.server.ts
@@ -0,0 +1,36 @@
+import { Players } from "@rbxts/services";
+import { $env } from "rbxts-transform-env";
+import { SupabaseStream } from "server/database/supabase";
+import { Users } from "shared/types/tables/users";
+
+const stream = new SupabaseStream(
+	`wss://${$env.string("PROJECT_ID")}.supabase.co/realtime/v1/websocket?apikey=${$env.string("SECRET_API_KEY")}`,
+	"public",
+);
+
+const leaderstats = new Map<Player, Instance[]>();
+
+Players.PlayerAdded.Connect((player) => {
+	const Leaderstats = new Instance("Folder");
+	Leaderstats.Name = "leaderstats";
+	Leaderstats.Parent = player;
+
+	const bwambles = new Instance("IntValue");
+	bwambles.Name = "Bwambles";
+	bwambles.Parent = Leaderstats;
+
+	leaderstats.set(player, [bwambles]);
+});
+
+stream.join<Users>("users", undefined, (event) => {
+	if (event.event !== "UPDATE" && event.event !== "DELETE" && event.event !== "INSERT") return;
+
+	const player = Players.GetPlayerByUserId(event.payload.record.id);
+	if (player) {
+		const bwambles = leaderstats.get(player)?.[0] as IntValue;
+
+		if (bwambles) {
+			bwambles.Value = event.payload.record.bwambles;
+		}
+	}
+});
