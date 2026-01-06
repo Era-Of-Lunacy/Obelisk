a893608925824a68e1a82693545435fcd64caaaa
Waffle <csshin9928@gmail.com>
2026-01-07 00:50:42 +0900

    fix: correct supabase url and data to upsert

--- src/server/common/player-handler.server.ts
+++ src/server/common/player-handler.server.ts
@@ -3,12 +3,11 @@ import { $env } from "rbxts-transform-env";
 import { SupabaseClient } from "server/database/supabase";
 
 if ($env.boolean("PROJECT_ID") && $env.boolean("API_KEY")) {
-	const client = new SupabaseClient($env.string("PROJECT_ID", ""), $env.string("API_KEY", ""));
+	const client = new SupabaseClient(`https://${$env.string("PROJECT_ID")}.supabase.co`, $env.string("API_KEY", ""));
 
 	Players.PlayerAdded.Connect((player) => {
 		const result = client.upsert("users", {
 			id: player.UserId,
-			username: player.Name,
 		});
 
 		if (!result.success) {
