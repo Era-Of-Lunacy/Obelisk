343e2fc3e5ffcd0ad4eb543f0d197ca80253153b
Waffle <csshin9928@gmail.com>
2026-01-08 03:42:57 +0900

    feat: add user class to leaderstats

--- src/server/handlers/leaderstats-handler.server.ts
+++ src/server/handlers/leaderstats-handler.server.ts
@@ -1,5 +1,6 @@
 import { Players } from "@rbxts/services";
 import { getUser, userUpdatedEvent } from "server/database/users";
+import { Class } from "shared/types/classes";
 
 const leaderstats = new Map<Player, Instance[]>();
 
@@ -11,12 +12,19 @@ Players.PlayerAdded.Connect((player) => {
 	const bwambles = new Instance("IntValue");
 	bwambles.Name = "Bwambles";
 	bwambles.Parent = Leaderstats;
+
+	const classType = new Instance("StringValue");
+	classType.Name = "Class";
+	classType.Parent = Leaderstats;
+
 	getUser(player)
 		.andThen((user) => {
 			if (user.data?.[0]) {
 				bwambles.Value = user.data[0].bwambles;
+				classType.Value = user.data[0].class;
 			} else {
 				bwambles.Value = 0;
+				classType.Value = Class.None;
 			}
 		})
 		.catch(() => {
@@ -24,7 +32,7 @@ Players.PlayerAdded.Connect((player) => {
 			bwambles.Value = 0;
 		});
 
-	leaderstats.set(player, [bwambles]);
+	leaderstats.set(player, [bwambles, classType]);
 });
 
 userUpdatedEvent.Connect((data) => {
@@ -32,9 +40,13 @@ userUpdatedEvent.Connect((data) => {
 		const player = Players.GetPlayerByUserId(data.id);
 		if (player) {
 			const bwambles = leaderstats.get(player)?.[0] as IntValue;
+			const classType = leaderstats.get(player)?.[1] as StringValue;
 
 			if (bwambles) {
 				bwambles.Value = data.bwambles ?? 0;
+			}
+			if (classType) {
+				classType.Value = data.class ?? Class.None;
 			}
 		}
 	}
