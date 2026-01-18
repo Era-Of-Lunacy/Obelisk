5f0e25a2f4981622fd246fa17885ff98e049fd6a
waffle <csshin9928@gmail.com>
2026-01-18 11:07:10 +0900

    feat: add user data handler with supabase integration

---
+++ src/server/handlers/data/user-data-handler.server.ts
@@ -0,0 +1,66 @@
+import { SupabaseClient } from "@rbxts/roblox-postgrest";
+import { Players } from "@rbxts/services";
+import { $env } from "rbxts-transform-env";
+import { Users } from "shared/types/users";
+
+const client = new SupabaseClient(
+	`https://${$env.string("PROJECT_ID")}.supabase.co`,
+	$env.string("SECRET_API_KEY", ""),
+);
+
+const cachedUsers: Record<number, Users> = {};
+
+Players.PlayerAdded.Connect((player) => {
+	const result = client.from("users").eq("id", player.UserId).maybeSingle<Users>();
+
+	if (result.success === true) {
+		if (result.data !== undefined) {
+			if (result.data.is_playing === true) {
+				player.Kick("User is already playing");
+				return;
+			}
+
+			if (result.data.deleted_at === undefined || result.data.deleted_at === "") {
+				cachedUsers[player.UserId] = result.data;
+
+				if (client.from("users").eq("id", player.UserId).update({ is_playing: true }).success === false) {
+					player.Kick("Failed to update user data");
+					return;
+				}
+			} else {
+				player.Kick("User is deleted");
+				return;
+			}
+		} else {
+			print("User not found Creating...");
+
+			const insertResult = client.from("users").insert<Users[]>({ id: player.UserId, is_playing: true });
+
+			if (insertResult.success === true && insertResult.data?.[0]) {
+				print("User created successfully");
+				cachedUsers[player.UserId] = insertResult.data[0];
+				return;
+			} else {
+				player.Kick("Failed to create user");
+				return;
+			}
+		}
+	}
+});
+
+Players.PlayerRemoving.Connect((player) => {
+	if (cachedUsers[player.UserId]) {
+		cachedUsers[player.UserId].is_playing = false;
+
+		if (
+			client
+				.from("users")
+				.eq("id", player.UserId)
+				.update({ ...cachedUsers[player.UserId] }).success === false
+		) {
+			print("Failed to update user data");
+		}
+
+		delete cachedUsers[player.UserId];
+	}
+});
