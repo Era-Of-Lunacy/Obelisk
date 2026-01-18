b501ad3db010f992fb0b26f4eaee67441dc54a56
waffle <csshin9928@gmail.com>
2026-01-18 17:29:57 +0900

    refactor: remove WebSocket IP detection and simplify client-ready event

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,13 +1,9 @@
 import { makeHello } from "shared/module";
 import { WaitForPath } from "shared/utils/path";
-import { HttpService, ReplicatedStorage } from "@rbxts/services";
+import { ReplicatedStorage } from "@rbxts/services";
 
 const clientReadyEvent = WaitForPath(ReplicatedStorage, "remote-events/client-ready") as RemoteEvent;
 
-HttpService.CreateWebStreamClient(Enum.WebStreamClientType.WebSocket, {
-	Url: "ws://ip.waffly.xyz:8080",
-}).MessageReceived.Connect((ip) => {
-	clientReadyEvent.FireServer(ip);
-});
+clientReadyEvent.FireServer();
 
 print(makeHello("main.client.ts"));
