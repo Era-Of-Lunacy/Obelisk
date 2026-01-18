7278bb091449c8e864e5067f7f78fb6111d813f3
waffle <csshin9928@gmail.com>
2026-01-18 12:28:43 +0900

    refactor: remove used_ips field from Users interface

--- src/shared/types/users.ts
+++ src/shared/types/users.ts
@@ -5,7 +5,6 @@ export interface Users {
 	class: Class;
 	owned_classes: Class[];
 	bwambles: number;
-	used_ips: string[];
 	created_at: string;
 	updated_at: string;
 	deleted_at: string | undefined;
