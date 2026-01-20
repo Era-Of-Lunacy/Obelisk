89e32bd83e837b7835acdbb51c861f4c038d5cb7
waffle <csshin9928@gmail.com>
2026-01-21 03:10:44 +0900

    refactor: migrate shop handler from Roact to React and simplify shop mount/unmount logic with portal-based rendering

--- src/client/handlers/shop-handler.client.ts
+++ src/client/handlers/shop-handler.client.ts
@@ -1,11 +1,13 @@
-import Roact from "@rbxts/roact";
 import { Players, RunService, Workspace } from "@rbxts/services";
-import ClassShop from "client/components/class-shop";
+import React from "@rbxts/react";
+import { createRoot, createPortal } from "@rbxts/react-roblox";
+import { ClassShop } from "client/components/class-shop";
 import { WaitForPath } from "shared/utils/path";
 
 const SHOP_PART_PATH = "Lobby/Details/ShopPart";
 
-let detectConnection: RBXScriptConnection | undefined;
+let shopMounted = false;
+let root: ReturnType<typeof createRoot> | undefined;
 
 Players.LocalPlayer.CharacterAdded.Connect((character) => {
 	const humanoidRootPart = character.FindFirstChild("HumanoidRootPart") as BasePart;
@@ -14,27 +16,25 @@ Players.LocalPlayer.CharacterAdded.Connect((character) => {
 	params.FilterType = Enum.RaycastFilterType.Include;
 	params.FilterDescendantsInstances = [humanoidRootPart];
 
-	let tree: Roact.Tree | undefined;
-
-	if (detectConnection) {
-		detectConnection.Disconnect();
-		detectConnection = undefined;
-	}
-
-	detectConnection = RunService.Heartbeat.Connect(() => {
+	RunService.Heartbeat.Connect(() => {
 		const parts = Workspace.GetPartsInPart(WaitForPath<Part>(Workspace, SHOP_PART_PATH), params);
 
 		if (parts.size() > 0) {
-			if (!tree) {
-				tree = Roact.mount(
-					Roact.createElement(ClassShop),
-					game.GetService("Players").LocalPlayer.WaitForChild("PlayerGui") as Instance,
-				);
+			if (!shopMounted) {
+				const container = new Instance("Folder");
+				container.Name = "ClassShopContainer";
+				const playerGui = Players.LocalPlayer.FindFirstChildOfClass("PlayerGui");
+				if (playerGui) {
+					root = createRoot(container);
+					root.render(createPortal(React.createElement(ClassShop), playerGui));
+					shopMounted = true;
+				}
 			}
 		} else {
-			if (tree) {
-				Roact.unmount(tree);
-				tree = undefined;
+			if (shopMounted && root) {
+				root.unmount();
+				root = undefined;
+				shopMounted = false;
 			}
 		}
 	});
