a81c2258a6d052988d0fa2fa00a2a47ca50d3f51
waffle <csshin9928@gmail.com>
2026-01-21 02:44:00 +0900

    refactor: remove class data cache from class handler and simplify to use user data module for class ownership and equipped class queries

--- src/client/handlers/class-handler.ts
+++ src/client/handlers/class-handler.ts
@@ -1,66 +1,36 @@
-import { getUserData } from "client/handlers/users-handler";
 import { ReplicatedStorage } from "@rbxts/services";
 import { WaitForPath } from "shared/utils/path";
-import { Classes } from "shared/types/classes";
+import { getUser } from "./data/users/user-data";
 
-const buyClassFunction = WaitForPath<RemoteFunction>(ReplicatedStorage, "remote-functions/buy-class");
-const equipClassFunction = WaitForPath<RemoteFunction>(ReplicatedStorage, "remote-functions/equip-class");
-const unequipClassFunction = WaitForPath<RemoteFunction>(ReplicatedStorage, "remote-functions/unequip-class");
-
-const classCreatedRemoteEvent = WaitForPath(ReplicatedStorage, "remote-events/class-created") as RemoteEvent<
-	(data: Classes & { class: string }) => void
->;
-const classUpdatedRemoteEvent = WaitForPath(ReplicatedStorage, "remote-events/class-updated") as RemoteEvent<
-	(data: Classes & { class: string }) => void
->;
-const classDeletedRemoteEvent = WaitForPath(ReplicatedStorage, "remote-events/class-deleted") as RemoteEvent<
-	(data: Classes & { class: string }) => void
->;
-
-const classData: Record<string, Classes> = {};
-
-classCreatedRemoteEvent.OnClientEvent.Connect((data) => {
-	if (data && data.class) {
-		classData[data.class] = data;
-	}
-});
-
-classUpdatedRemoteEvent.OnClientEvent.Connect((data) => {
-	if (data && data.class) {
-		classData[data.class] = { ...classData[data.class], ...data };
-	}
-});
-
-classDeletedRemoteEvent.OnClientEvent.Connect((data) => {
-	if (data && data.class) {
-		delete classData[data.class];
-	}
-});
-
-export function getClassData(): Record<string, Classes> {
-	return classData;
-}
-
-export function getClass(className: string): Classes | undefined {
-	return classData[className];
-}
+const buyClassFunction = WaitForPath<RemoteFunction<(classType: string) => boolean>>(
+	ReplicatedStorage,
+	"remote-functions/buy-class",
+);
+const equipClassFunction = WaitForPath<RemoteFunction<(classType: string) => boolean>>(
+	ReplicatedStorage,
+	"remote-functions/equip-class",
+);
+const unequipClassFunction = WaitForPath<RemoteFunction<() => boolean>>(
+	ReplicatedStorage,
+	"remote-functions/unequip-class",
+);
 
 export function hasClass(classType: string): boolean {
-	return getUserData().owned_classes?.includes(classType) ?? false;
+	return getUser().owned_classes?.includes(classType) ?? false;
 }
 
 export function getEquippedClass(): string {
-	return getUserData().class ?? "None";
+	return getUser().class ?? "None";
 }
 
 export function buyClass(classType: string): boolean {
-	return buyClassFunction.InvokeServer(classType) as boolean;
+	return buyClassFunction.InvokeServer(classType);
 }
 
 export function equipClass(classType: string): boolean {
-	return equipClassFunction.InvokeServer(classType) as boolean;
+	return equipClassFunction.InvokeServer(classType);
 }
 
 export function unequipClass(): boolean {
-	return unequipClassFunction.InvokeServer() as boolean;
+	return unequipClassFunction.InvokeServer();
 }
