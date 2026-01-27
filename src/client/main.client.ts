9609de51ae1138c4fde9a1552953a54cda84edc7
Waffle0823 <csshin9928@gmail.com>
2026-01-28 00:08:57 +0900

    feat: initialize flamework for both client and server side

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,9 +1,12 @@
 import { makeHello } from "shared/module";
 import { WaitForPath } from "shared/utils/path";
 import { ReplicatedStorage } from "@rbxts/services";
+import { Flamework } from "@flamework/core";
 
 const clientReadyEvent = WaitForPath(ReplicatedStorage, "remote-events/client-ready") as RemoteEvent;
 
 clientReadyEvent.FireServer();
 
 print(makeHello("main.client.ts"));
+
+Flamework.ignite();
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,3 +1,6 @@
+import { Flamework } from "@flamework/core";
 import { makeHello } from "shared/module";
 
 print(makeHello("main.server.ts"));
+
+Flamework.ignite();
