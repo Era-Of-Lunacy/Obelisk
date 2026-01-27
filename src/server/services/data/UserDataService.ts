5b8118645b13df6930e9a666489393be0f8e950e
Waffle0823 <csshin9928@gmail.com>
2026-01-28 07:38:20 +0900

    feat: implement UserDataService with loading, saving data

---
+++ src/server/services/data/UserDataService.ts
@@ -0,0 +1,167 @@
+import { OnStart, Service } from "@flamework/core";
+import { SupabaseClient } from "@rbxts/roblox-supabase";
+import { HttpService, Players } from "@rbxts/services";
+import { $env } from "rbxts-transform-env";
+import { DataCache } from "server/types/data";
+import { Database } from "shared/types/database.types";
+
+export type User = Database["public"]["Tables"]["users"]["Row"];
+
+const SAVE_INTERVAL = 60;
+const RETRY_COUNT = 10;
+const RETRY_DELAY = 0.1;
+
+@Service()
+export default class UserDataService implements OnStart {
+	private supabase: SupabaseClient<Database>;
+	private cachedUserData: Map<number, DataCache<User>> = new Map();
+
+	constructor() {
+		this.supabase = new SupabaseClient<Database>(
+			`https://${$env.string("PROJECT_ID")}.supabase.co`,
+			HttpService.GetSecret("SUPABASE_ANON_KEY"),
+		);
+	}
+
+	private loadData(player: Player): void {
+		let cache = this.cachedUserData.get(player.UserId);
+
+		// Skip if cache is in saving
+		if (cache?.status === "saving") return;
+
+		task.spawn(() => {
+			// Wait for other process to finish loading
+			// We can loop though cache is not exist
+			while (cache?.status === "loading") {
+				task.wait();
+			}
+
+			// Set cache status to loading
+			if (cache) {
+				cache.status = "loading";
+			} else {
+				this.cachedUserData.set(player.UserId, {
+					status: "loading",
+					data: undefined,
+				});
+
+				cache = this.cachedUserData.get(player.UserId);
+			}
+
+			// Upsert data
+			Promise.retryWithDelay(
+				() =>
+					this.supabase
+						.from("users")
+						.upsert({ id: player.UserId, is_playing: true }, { returning: "representation" })
+						.single()
+						.andThen((resolve) => {
+							if (resolve.error) return Promise.reject(resolve.error);
+
+							this.cachedUserData.set(player.UserId, {
+								status: "ready",
+								data: resolve.data,
+							});
+						}),
+				RETRY_COUNT,
+				RETRY_DELAY,
+			)
+				.andThen(() => {
+					print("Loaded user data for player: ", player.UserId);
+				})
+				.catch((err) => {
+					this.cachedUserData.get(player.UserId)!.status = "error";
+
+					print("An error occurred while loading user data: ", err);
+					player.Kick("Failed to load user data");
+				});
+		});
+	}
+
+	private saveData(player: Player): void {
+		const cache = this.cachedUserData.get(player.UserId);
+
+		// Skip if cache is already in saving or doesn't exist
+		if (!cache || cache.status === "saving") return;
+
+		task.spawn(() => {
+			// Wait for cache to be loaded
+			while (cache.status === "loading") {
+				task.wait();
+			}
+
+			// Set cache status to saving
+			cache.status = "saving";
+
+			// Don't have to save data if it doesn't exist
+			if (cache.data === undefined) {
+				this.cachedUserData.delete(player.UserId);
+				return;
+			}
+
+			// Save data
+			Promise.retryWithDelay(
+				() =>
+					this.supabase
+						.from("users")
+						// We've already checked if cache.data exists in line 84
+						// And cache.data cannot be undefined due to "loading" flag
+						.update(cache.data!)
+						.eq("id", player.UserId)
+						.execute()
+						.andThen((response) => {
+							if (response.error) return Promise.reject(response.error);
+						}),
+				RETRY_COUNT,
+				RETRY_DELAY,
+			)
+				.andThen(() => {
+					print("Saved user data for player: ", player.UserId);
+				})
+				.catch((err) => {
+					print("Error occurred while saving user data: ", err);
+				})
+				.finally(() => {
+					// Clear cache
+					// WARN | This will cause data loss if player is not saved
+					this.cachedUserData.delete(player.UserId);
+				});
+		});
+	}
+
+	private saveAllData() {
+		for (const [userId] of this.cachedUserData) {
+			const player = Players.GetPlayerByUserId(userId);
+
+			if (player) {
+				this.saveData(player);
+			}
+		}
+	}
+
+	onStart(): void {
+		Players.PlayerAdded.Connect((player) => {
+			this.loadData(player);
+		});
+
+		Players.PlayerRemoving.Connect((player) => {
+			this.saveData(player);
+		});
+
+		task.spawn(() => {
+			while (true) {
+				task.wait(SAVE_INTERVAL);
+
+				this.saveAllData();
+			}
+		});
+
+		game.BindToClose(() => {
+			this.saveAllData();
+
+			while (this.cachedUserData.size() > 0) {
+				task.wait();
+			}
+		});
+	}
+}
