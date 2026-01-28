31608f4b47c2c4ef9098b50a133d9d3f8aa3a78a
ChanSoo Shin <csshin9928@gmail.com>
2026-01-29 06:11:28 +0900

    feat: add class data update events to client-server networking interface

--- src/shared/networking/Data.ts
+++ src/shared/networking/Data.ts
@@ -1,16 +1,20 @@
 import { Networking } from "@flamework/networking";
+import { DatabaseEvents } from "shared/types/database";
 import { Database } from "shared/types/database.types";
 
 type UserUpdate = Database["public"]["Tables"]["users"]["Update"];
+type ClassUpdate = Database["public"]["Tables"]["classes"]["Update"];
 
 interface ClientToServerEvents {
 	clientReady(): void;
 	userDataUpdated(data: UserUpdate): void;
+	classDataUpdated(event: DatabaseEvents, data: ClassUpdate): void;
 }
 
 interface ServerToClientEvents {
 	clientReady(): void;
 	userDataUpdated(data: UserUpdate): void;
+	classDataUpdated(event: DatabaseEvents, data: ClassUpdate): void;
 }
 
 export const GlobalDataEvents = Networking.createEvent<ClientToServerEvents, ServerToClientEvents>();
