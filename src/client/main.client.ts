4a4a200d3b31de4a28d7d5c847441b3dad98df38
ChanSoo Shin <csshin9928@gmail.com>
2026-01-30 01:57:19 +0900

    refactor: add client components path to Flamework initialization

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -5,6 +5,7 @@ import { GlobalDataEvents } from "shared/networking/Data";
 print(makeHello("main.client.ts"));
 
 Flamework.addPaths("src/client/controllers");
+Flamework.addPaths("src/client/components");
 Flamework.ignite();
 
 GlobalDataEvents.createClient({}).clientReady.fire();
