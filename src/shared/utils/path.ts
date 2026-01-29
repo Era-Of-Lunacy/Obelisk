55f3b31cffd6838b7ae43a7cf3746d98a9ae92d3
ChanSoo Shin <csshin9928@gmail.com>
2026-01-30 00:03:26 +0900

    refactor: remove unused WaitForPath utility function

--- src/shared/utils/path.ts
+++
@@ -1,9 +0,0 @@
-export function WaitForPath<T extends Instance>(root: Instance, path: string): T {
-	let current = root;
-
-	for (const name of path.split("/")) {
-		current = current.WaitForChild(name);
-	}
-
-	return current as T;
-}
