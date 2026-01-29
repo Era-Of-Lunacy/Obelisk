61ff70e023efb82e26a48b4abaae320c2f3fa6d6
ChanSoo Shin <csshin9928@gmail.com>
2026-01-29 23:50:32 +0900

    refactor: replace WaitForPath with Flamework networking for client ready event

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,12 +1,10 @@
 import { makeHello } from "shared/module";
-import { WaitForPath } from "shared/utils/path";
-import { ReplicatedStorage } from "@rbxts/services";
 import { Flamework } from "@flamework/core";
-
-const clientReadyEvent = WaitForPath(ReplicatedStorage, "remote-events/client-ready") as RemoteEvent;
-
-clientReadyEvent.FireServer();
+import { GlobalDataEvents } from "shared/networking/Data";
 
 print(makeHello("main.client.ts"));
 
+Flamework.addPaths("src/client/controllers");
 Flamework.ignite();
+
+GlobalDataEvents.createClient({}).clientReady.fire();
