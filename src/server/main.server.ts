912deef782f2587157b249556bb242c911f2ef27
ChanSoo Shin <csshin9928@gmail.com>
2026-01-29 23:50:08 +0900

    feat: add controllers and components paths to Flamework initialization

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -4,4 +4,6 @@ import { makeHello } from "shared/module";
 print(makeHello("main.server.ts"));
 
 Flamework.addPaths("src/server/services");
+Flamework.addPaths("src/server/controllers");
+Flamework.addPaths("src/server/components");
 Flamework.ignite();
