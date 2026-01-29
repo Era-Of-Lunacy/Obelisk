25b61bea8d3cdece96751d2de9382c9dcdcd1529
ChanSoo Shin <csshin9928@gmail.com>
2026-01-29 23:57:23 +0900

    feat: add database table type definitions for User and Class entities

--- src/shared/types/database.ts
+++ src/shared/types/database.ts
@@ -1,5 +1,22 @@
+import { Database } from "./database.types";
+
 export enum DatabaseEvents {
 	Created,
 	Updated,
 	Deleted,
 }
+
+/**
+ * User Table types
+ */
+export type User = Database["public"]["Tables"]["users"]["Row"];
+export type UserInsert = Database["public"]["Tables"]["users"]["Insert"];
+export type UserUpdate = Database["public"]["Tables"]["users"]["Update"];
+
+/**
+ * Class Table types
+ */
+export type Class = Database["public"]["Tables"]["classes"]["Row"];
+export type ClassInsert = Database["public"]["Tables"]["classes"]["Insert"];
+export type ClassUpdate = Database["public"]["Tables"]["classes"]["Update"];
+export type ClassType = Database["public"]["Enums"]["class"];
