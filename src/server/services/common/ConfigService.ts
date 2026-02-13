b7f2bcb745fc7327ecb2fb65a9999577003686c0
Waffle <csshin9928@gmail.com>
2026-02-13 23:27:55 +0900

    feat: safety check when loading config

--- src/server/services/common/ConfigService.ts
+++ src/server/services/common/ConfigService.ts
@@ -1,25 +1,34 @@
 import { Flamework, Service } from "@flamework/core";
+import { $warn } from "rbxts-transform-debug";
 
 @Service()
 export default class ConfigService {
-  private snapshot;
+  private snapshot: ConfigSnapshot | undefined;
 
   constructor() {
-    this.snapshot = game.GetService("ConfigService").GetConfigAsync();
+    // TODO: Retry while success to load config
+    try {
+      const snapshot = game.GetService("ConfigService").GetConfigAsync();
+      this.snapshot = snapshot;
 
-    this.snapshot.UpdateAvailable.Connect(() => {
-      this.snapshot.Refresh();
-    });
+      this.snapshot.UpdateAvailable.Connect(() => {
+        snapshot.Refresh()
+      })
+    } catch {
+      $warn("Error while getting config")
+    }
   }
 
   getConfig<T>(name: string):
     | { success: true; value: T }
     | { success: false; value?: undefined } {
-      const value = this.snapshot.GetValue(name) as T;
-      const guard = Flamework.createGuard<T>();
+    if (!this.snapshot) return { success: false, value: undefined };
 
-      if (guard(value)) return { success: true, value: value }
+    const value = this.snapshot.GetValue(name) as T;
+    const guard = Flamework.createGuard<T>();
 
-      return {success: false, value: undefined};
+    if (guard(value)) return { success: true, value: value }
+
+    return { success: false, value: undefined };
   }
 }
