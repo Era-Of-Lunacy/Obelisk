25b4b0e8dd025f489c53ebe36d7873239cb6f179
Waffle <csshin9928@gmail.com>
2026-02-13 18:36:42 +0900

    feat: add type checking of config

--- src/server/services/common/ConfigService.ts
+++ src/server/services/common/ConfigService.ts
@@ -1,18 +1,25 @@
-import { Service } from "@flamework/core";
+import { Flamework, Service } from "@flamework/core";
 
 @Service()
 export default class ConfigService {
-	private snapshot;
+  private snapshot;
 
-	constructor() {
-		this.snapshot = game.GetService("ConfigService").GetConfigAsync();
+  constructor() {
+    this.snapshot = game.GetService("ConfigService").GetConfigAsync();
 
-		this.snapshot.UpdateAvailable.Connect(() => {
-			this.snapshot.Refresh();
-		});
-	}
+    this.snapshot.UpdateAvailable.Connect(() => {
+      this.snapshot.Refresh();
+    });
+  }
 
-	getConfig<T>(name: string): T {
-		return this.snapshot.GetValue(name) as T;
-	}
+  getConfig<T>(name: string):
+    | { success: true; value: T }
+    | { success: false; value?: undefined } {
+      const value = this.snapshot.GetValue(name) as T;
+      const guard = Flamework.createGuard<T>();
+
+      if (guard(value)) return { success: true, value: value }
+
+      return {success: false, value: undefined};
+  }
 }
