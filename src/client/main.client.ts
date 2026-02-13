c406823a36fe3c7861fbc9579237dd36552fefd5
Waffle <csshin9928@gmail.com>
2026-02-13 20:50:05 +0900

    refactor: remove useless debug

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,8 +1,5 @@
-import { makeHello } from "shared/module";
 import { Flamework } from "@flamework/core";
 import { GlobalDataEvents } from "shared/networking/Data";
-
-print(makeHello("main.client.ts"));
 
 Flamework.addPaths("src/client/controllers");
 Flamework.addPaths("src/client/components");
