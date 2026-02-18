ae5bc4ce3c762763631cf934159b22d3c9c6c5dc
ChanSoo Shin <csshin9928@gmail.com>
2026-02-18 12:27:25 +0900

    feat: add user role enum

--- src/shared/types/database.d.ts
+++ src/shared/types/database.d.ts
@@ -1,10 +1,16 @@
 import { Database } from "@include/database";
 
 export enum DatabaseEvents {
-	Created,
-	Updated,
-	Deleted,
+  Created,
+  Updated,
+  Deleted,
 }
+
+/**
+ * Enums
+ */
+export type Role = Database["public"]["Enums"]["role"];
+export type Class = Database["public"]["Tables"]["classes"]["Row"];
 
 /**
  * User Table types
@@ -16,7 +22,6 @@ export type UserUpdate = Database["public"]["Tables"]["users"]["Update"];
 /**
  * Class Table types
  */
-export type Class = Database["public"]["Tables"]["classes"]["Row"];
 export type ClassInsert = Database["public"]["Tables"]["classes"]["Insert"];
 export type ClassUpdate = Database["public"]["Tables"]["classes"]["Update"];
 export type ClassType = Database["public"]["Enums"]["class"];
