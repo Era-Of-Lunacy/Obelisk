d5b6e6ddfb3bb97dd343c4a4f647534382c72a63
Waffle <csshin9928@gmail.com>
2026-03-04 16:32:29 +0900

    build: migrate eslint config file to newer version

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import robloxTs from "eslint-plugin-roblox-ts";
+import prettier from "eslint-plugin-prettier";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default defineConfig([
+    globalIgnores(["out"]),
+    {
+        files: ["**/*.js", "**/*.mjs"],
+    },
+    {
+        extends: compat.extends(
+            "eslint:recommended",
+            "plugin:@typescript-eslint/recommended",
+            "plugin:roblox-ts/recommended-legacy",
+            "plugin:prettier/recommended",
+        ),
+
+        files: ["**/*.ts", "**/*.tsx"],
+
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2018,
+            sourceType: "module",
+
+            parserOptions: {
+                jsx: true,
+                useJSXTextNode: true,
+                project: "./tsconfig.json",
+            },
+        },
+
+        rules: {
+            "prettier/prettier": "warn",
+        },
+    },
+]);
